e1321f09d586dd6ee42c2ec8aa6acc38940c29b4
Max Jaecklein <maximilian.jaecklein@tum.de>
2025-08-26 21:37:28 +0000

    Change Scripts
    Signed-off-by: Max Jaecklein <maximilian.jaecklein@tum.de>

---
+++ copy-modules.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "Missing image name."
+    echo "Usage: ./copy-modules.sh <Path of image>"
+    exit 1
+fi
+
+linux_dir="./spdm-linux"
+image=$1
+qemu_dir="$linux_dir/qemu_edu"
+
+nix-shell -p gnumake --run "make -C $qemu_dir"
+mkdir mount-point
+sudo mount -o loop $image mount-point
+sudo cp $qemu_dir/*.ko mount-point/modules/
+sudo umount mount-point
+rm -rdf mount-point
+
